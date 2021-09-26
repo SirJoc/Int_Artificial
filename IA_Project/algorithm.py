4af320ec51a6992fc3a9935ac1bd9731ab43d120
SirJoc <josiasolaya2016@gmail.com>
2021-09-26 00:55:14 -0500

    V4

--- IA_Project/algorithm.py
+++ IA_Project/algorithm.py
@@ -1,4 +1,6 @@
 arr = [[1,2,3],[4,5,6],[7,8,9]]
+estados = []
+estados.append(arr)
 DIRECTIONS = {"U" : (-1, 0), "D": [1, 0], "L": [0, -1], "R": [0, 1]}
 print(DIRECTIONS)
 
@@ -19,6 +21,8 @@ if sol[0] < 0 or sol[0] > 2 or sol[1] < 0 or sol[1] > 2:
     print("No jugada")
 else :
     print(sol)
+    arr = [row[:] for row in arr]
     swapT(arr, (j,k), sol)
-    print(arr)
+    estados.append(arr)
+    print(estados)
 
