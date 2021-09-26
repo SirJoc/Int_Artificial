e3ef9ecd93e8c9d580352d91d3a2bebb5559e7db
SirJoc <josiasolaya2016@gmail.com>
2021-09-25 22:32:39 -0500

    V3

---
+++ IA_Project/algorithm.py
@@ -0,0 +1,24 @@
+arr = [[1,2,3],[4,5,6],[7,8,9]]
+DIRECTIONS = {"U" : (-1, 0), "D": [1, 0], "L": [0, -1], "R": [0, 1]}
+print(DIRECTIONS)
+
+j = 0
+k = 0
+
+def swapT(arr, posI, posF):
+    aux = arr[posI[0]][posI[1]]
+    arr[posI[0]][posI[1]] = arr[posF[0]][posF[1]]
+    arr[posF[0]][posF[1]] = aux
+
+
+def sumT(t1, t2):
+    return (t1[0]+t2[0], t1[1]+t2[1])
+
+sol = sumT((j,k),(DIRECTIONS["R"]))
+if sol[0] < 0 or sol[0] > 2 or sol[1] < 0 or sol[1] > 2:
+    print("No jugada")
+else :
+    print(sol)
+    swapT(arr, (j,k), sol)
+    print(arr)
+
--- IA_Project/main.py
+++ IA_Project/main.py
@@ -1,4 +1,4 @@
-import algorithm
+#import algorithm
 import objects
 import pygame
 
@@ -13,44 +13,50 @@ screen = pygame.display.set_mode((420, 420))
 pygame.display.set_caption("8 puzzles")
 pygame.display.set_icon(icon)
 
-arr = []
-for i in range(9):
-    arr.append(i)
+arr = [[1,2,3],[4,5,6],[7,8,0]]
+DIRECTIONS = {"U" : (-1, 0), "D": [1, 0], "L": [0, -1], "R": [0, 1]}
+imagenes = [[], [], []]
+def generarImg():
+    posY = 0
+    posX = 0
+    for i in range(3):
+        if i == 1:
+            posY = 140
+        elif i == 2:
+            posY = 280
 
-imagenes = []
-posY = 0
-posX = 0
+        for j in range(3):
+            if j == 0:
+                posX = 0
+            elif j == 1:
+                posX = 140
+            elif j == 2:
+                posX = 280
 
-for i in range(9):
-    if i < 3:
-        posY = 0
-        posX += 140
-    elif i < 6:
-        posY = 140
-        posX += 140
-    else:
-        posY = 280
-        posX += 140
+            obj = objects.Img()
+            obj.setPos(posX, posY)
 
-    if i == 0 or i == 3 or i == 6:
-        posX = 0
+            obj.setName(arr[i][j])
+            imagenes[i].append(obj)
 
-    obj = objects.Img()
-    obj.setPos(posX, posY)
-    obj.setName(arr[i])
-    imagenes.append(obj)
+
 
 #ejemplo screen.blit(squareImg, (imgX, imgY))
 
 # GAME LOOP --> makes game function properly
 running = True
+generarImg()
+print(imagenes)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     # Background
     screen.fill((155, 155, 155))
-    for i in range(9):
-        screen.blit(imagenes[i].getImg(), imagenes[i].getPos())
+    for i in range(3):
+        for j in range(3):
+            # para hacer el swap, solo se debe cambiar el name --> imagenes[i].setName(1)
+            screen.blit(imagenes[i][j].getImg(), imagenes[i][j].getPos())
+
     # aqui se pone el proceso de dibujado
     pygame.display.update()
